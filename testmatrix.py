01b29266d895673b18f7f46452431021101ab83e
KoenPrudhommevanReine <koen.prudhomme@ziggo.nl>
2024-01-17 10:23:32 +0100

    Improved time formatting

--- testmatrix.py
+++ testmatrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools
 import pandas as pd
+import datetime
 
 def get_test_matrix():
     dummy = [n_elevator, n_windspeed, n_prop, n_angles]
@@ -43,34 +44,6 @@ def wind_off(na):
     return (a * (c_alpha - 1) + prop_onoff) / 60  # return time in minutes
 
 def time_estimate(df):
-    diff = lambda param, i: df[param][i] - df[param][i-1]
-    point_time_list = [0]
-    total_time_list = [0]
-    for i in range(1, len(df)): # starting from the second datapoint
-        AoA_diff = diff("AoA", i)
-        elevator_diff = diff("Elevator", i)
-        velocity_diff = diff("Tunnel velocity", i)
-        propset_diff = diff("propeller setting", i)
-        point_time = abs(AoA_diff) * 2 + 15
-        if velocity_diff != 0:
-            point_time += 60  # 60 seconds to change the speed
-        if elevator_diff != 0:
-            point_time += 12.5*60 + 3*60  # 12.5 min of downtime + 3 min to get going again
-        if propset_diff != 0:
-            point_time += 30
-        point_time_list.append(point_time)
-        total_time = np.sum(point_time_list)
-        total_time_list.append(total_time)
-    df["setpoint time"] = point_time_list
-    df["total time"] = total_time_list
-    # df["setpoint time"] *= 1/ 60
-    # df["total time"] *= 1/60
-    return df
-
-import pandas as pd
-import numpy as np
-
-def time_estimate2(df):
     # Calculate differences
     AoA_diff = df["AoA"].diff().abs()
     elevator_diff = df["Elevator"].diff().abs()
@@ -88,24 +61,27 @@ def time_estimate2(df):
     point_time.iloc[0] = 0  # or any value you want
     total_time.iloc[0] = 0  # or any value you want
 
-    # Add new columns to the DataFrame
-    df["setpoint time"] = point_time
-    df["total time"] = total_time
+    def convert_to_hms(series):
+        return series.apply(lambda x: pd.to_timedelta(x, unit='s').components).apply(
+            lambda x: '{:02}:{:02}:{:02}'.format(x.hours, x.minutes, x.seconds))
+
+    df["setpoint time"] = convert_to_hms(point_time)
+    df["total time"] = convert_to_hms(total_time)
 
     return df
 
 
 
 
-# n_angles = [-5, 7, 12, 14]  # [deg]
-# n_elevator = [-15, 0, 15]  # [deg]
-# n_windspeed = [10, 20, 40]  # [m/s]
-# n_prop = [0, 1, 2]  # [rpm]
-
 n_angles = [-5, 7, 12, 14]  # [deg]
-n_elevator = [0,15]  # [deg]
-n_windspeed = [40]  # [m/s]
-n_prop = [2]  # [rpm]
+n_elevator = [-15, 0, 15]  # [deg]
+n_windspeed = [10, 20, 40]  # [m/s]
+n_prop = [0, 1, 2]  # [rpm]
+
+# n_angles = [-5, 7, 12, 14]  # [deg]
+# n_elevator = [0,15]  # [deg]
+# n_windspeed = [40]  # [m/s]
+# n_prop = [2]  # [rpm]
 
 # 15+5 for tunnel prep, 10 for trimming
 total_time = wind_on(n_angles, n_prop, n_windspeed, n_elevator) + wind_off(n_angles) + 15 + 5 + 10
@@ -114,5 +90,4 @@ print('total time=', total_time, 'minutes')
 testmatrix = get_test_matrix()
 
 print(time_estimate(testmatrix))
-print(time_estimate2(testmatrix))
 print(f"3 hours = {3*60*60} seconds")
