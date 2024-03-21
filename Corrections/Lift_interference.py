f894dcbafabdea339b73fc0b47df4f9f453c39fe
KoenPrudhommevanReine <koen.prudhomme@ziggo.nl>
2024-03-21 17:19:28 +0100

    Updated lift interference to properly read tail off data, made plotting in separate file for cleanliness

--- Corrections/Lift_interference.py
+++ Corrections/Lift_interference.py
@@ -1,22 +1,29 @@
 import pandas as pd
 
-def import_tailoff():
+
+def df_velocity_filter(V_target: int):
     df = pd.read_excel("TailOffData.xlsx", sheet_name="AoS = 0 deg")
     df = df.drop("AoS", axis=1)
-    return df
-
-def df_velocity_filter(df, V_target: int):
     margin = 0.1  # [m/s]
-    filtered_df = df[(df['Vinf'] >= V_target-margin) & (df['Vinf'] <= V_target+margin)]
+    filtered_df = df[(df['Vinf'] >= V_target - margin) & (df['Vinf'] <= V_target + margin)]
+    filtered_df = filtered_df.reset_index(drop=True)
     return filtered_df
 
 
-df = import_tailoff()
-print(df)
-df_V20 = df_velocity_filter(df, 20)
-print(df_V20)
+def average_40(df_original):
+    # Get the first 11 rows and the last 11 rows
+    first_half = df_original.iloc[:11]
+    second_half = df_original.iloc[20:31]
 
+    # Reset the index of the second half so it matches with the first half
+    second_half.reset_index(drop=True, inplace=True)
 
+    # Calculate the average
+    average_df = (first_half + second_half) / 2
+
+    df_new = pd.concat([df_original.iloc[11:20], average_df], ignore_index=True, sort=False)
+    df_return = df_new.sort_values(by=['AoA'], ascending=True)
+    return df_return.reset_index(drop=True)
 
 
 
