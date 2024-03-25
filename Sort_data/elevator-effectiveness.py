877cf4e2636445726d81a0ffee1b6b0a67868a55
dorotheruys <dorothe@beruys.be>
2024-03-25 10:27:36 +0100

    made the scripts into functions for clarity

--- Sort_data/elevator-effectiveness.py
+++ Sort_data/elevator-effectiveness.py
@@ -17,6 +17,38 @@ def get_aoa_combis(aoa):
     return combis
 
 
+def get_cm_vs_elevator(cm_datapoints):
+    # Extract each CM from the data (and reset the indices of the zero angle)
+    CM_min15 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [-15]*len(cm_datapoints)}), bal_sorted_min15['CMpitch']], axis=1)
+    CM_0 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [0]*len(cm_datapoints)}), bal_sorted_0_sliced['CMpitch'].reset_index(drop=True)], axis=1)
+    CM_15 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [15]*len(cm_datapoints)}), bal_sorted_15['CMpitch']], axis=1)
+
+    # Make one large dataframe of all relevant data points to construct the graph
+    CM_df = pd.concat([CM_min15, CM_0, CM_15], axis=0, ignore_index=True)
+
+    # Plot
+    cm_deltae_plotting_lst = get_function_from_dataframe(CM_df, 2, 'delta_e', 'CMpitch', get_aoa_combis(7), np.linspace(-20, 20, 50), f'$\\delta_e$ [deg]', r'$C_M$ [-]')
+
+    return cm_deltae_plotting_lst
+
+
+def get_slope_cm_vs_aoa(cm_datapoints):
+    # Make an array with all Cm coefficients and polyfit to line to return the slopes aka Cm delta
+    CM_array = np.array([bal_sorted_min15['CMpitch'], bal_sorted_0_sliced['CMpitch'], bal_sorted_15['CMpitch']])
+    delta_e_array = [-15, 0, 15]    # deg
+    coeff_CM = np.polyfit(np.transpose(delta_e_array), CM_array, 1)
+
+    # Make a dataframe consisting the slopes, AoA, V and K
+    cm_slope = pd.DataFrame(data=({'CM_de': coeff_CM[0]}))
+
+    dcm_dataframe = pd.concat([cm_datapoints, cm_slope], axis=1)
+
+    # Get plot for AoA versus dCM/d delta_e, for each V & J combination
+    dcm_ddeltae_plotting_lst = get_function_from_dataframe(dcm_dataframe, 2, 'AoA', 'CM_de', tunnel_prop_combi, np.linspace(-6, 20, 26), f'$\\alpha$ [deg]', r'$\frac{\partial C_M}{\partial \delta_e}$ [-]')
+
+    return dcm_ddeltae_plotting_lst
+
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 file1 = os.path.join(current_dir, 'bal_sorted1.csv')
@@ -41,35 +73,14 @@ rows = [0, 12, 17, 19, 20, 21, 22, 23, 24, 26, 27, 28, 29, 42, 47, 49]
 bal_sorted_0_sliced1 = bal_sorted_0.iloc[rows]
 bal_sorted_0_sliced = pd.concat([bal_sorted_0_sliced1, bal_sorted_0[50:]])
 
-# # # 1. Get CM vs delta_e curve
 # Get the datapoints at which the CM were made
-cm_datapoints = bal_sorted_15.loc[:, ['AoA', 'rounded_AoA', 'V', 'rounded_v', 'J_M1', 'rounded_J']]
+cm_data_points = bal_sorted_15.loc[:, ['AoA', 'rounded_AoA', 'V', 'rounded_v', 'J_M1', 'rounded_J']]
 
-# Extract each CM from the data (and reset the indices of the zero angle)
-CM_min15 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [-15]*len(cm_datapoints)}), bal_sorted_min15['CMpitch']], axis=1)
-CM_0 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [0]*len(cm_datapoints)}), bal_sorted_0_sliced['CMpitch'].reset_index(drop=True)], axis=1)
-CM_15 = pd.concat([cm_datapoints, pd.DataFrame({'delta_e': [15]*len(cm_datapoints)}), bal_sorted_15['CMpitch']], axis=1)
+# 1. Get CM vs delta_e curve
+get_cm_vs_elevator(cm_data_points)
 
-# Make one large dataframe of all relevant data points to construct the graph
-CM_df = pd.concat([CM_min15, CM_0, CM_15], axis=0, ignore_index=True)
-
-# Plot
-cm_deltae_plotting_lst = get_function_from_dataframe(CM_df, 2, 'delta_e', 'CMpitch', get_aoa_combis(7), np.linspace(-20, 20, 50), f'$\\delta_e$ [deg]', r'$C_M$ [-]')
-
-# # # 2. Get dCM/d delta_e vs AoA curve
-# Make an array with all Cm coefficients and polyfit to line to return the slopes aka Cm delta
-CM_array = np.array([bal_sorted_min15['CMpitch'], bal_sorted_0_sliced['CMpitch'], bal_sorted_15['CMpitch']])
-delta_e_array = [-15, 0, 15]    # deg
-coeff_CM = np.polyfit(np.transpose(delta_e_array), CM_array, 1)
-
-# Make a dataframe consisting the slopes, AoA, V and K
-cm_slope = pd.DataFrame(data=({'CM_de': coeff_CM[0]}))
-
-dcm_dataframe = pd.concat([cm_datapoints, cm_slope], axis=1)
-
-# Get plot for AoA versus dCM/d delta_e, for each V & J combination
-dcm_ddeltae_plotting_lst = get_function_from_dataframe(dcm_dataframe, 2, 'AoA', 'CM_de', tunnel_prop_combi, np.linspace(-6, 20, 26), f'$\\alpha$ [deg]', r'$\frac{\partial C_M}{\partial \delta_e}$ [-]')
-
+# 2. Get dCM/d delta_e vs AoA curve
+get_slope_cm_vs_aoa(cm_data_points)
 
 # # Get plot for AoA vs CL, for each V & J combination
 # get_function_from_dataframe(bal_sorted_15, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), f'$\\alpha$ [deg]', f'$C_L$ [-]')
