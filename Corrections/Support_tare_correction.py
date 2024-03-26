5c173fc325a79ef3410c443df7de317d15a8ab4d
KoenPrudhommevanReine <koen.prudhomme@ziggo.nl>
2024-03-26 14:34:01 +0100

    Strut corrections

--- Corrections/Support_tare_correction.py
+++ Corrections/Support_tare_correction.py
@@ -7,15 +7,22 @@ def strut_correction():
     df = df.rename(columns={"AoA [deg]": "AoA"})
     return df
 
-def main():
-    path = get_file_path("bal_sorted2.csv", folder="Sort_data")
-    df_uncor = pd.read_csv(path)
+def add_modeloff_corrections():
+    df = pd.read_csv(get_file_path("bal_sorted2.csv", folder="Sort_data"))
     df_strut = strut_correction()
 
+    df_strut_aoa = pd.DataFrame()  # Initialize as an empty DataFrame
+    for aoa in df["rounded_AoA"]:
+        df_strut_aoa = pd.concat([df_strut_aoa, df_strut[df_strut["AoA"] == aoa]], ignore_index=True)
+    df_strut_aoa.reset_index(drop=True, inplace=True)
 
-    for aoa in df_uncor["rounded_AoA"]:
-        df_strut_aoa.loc[-1] = = df_strut[(df_strut["AoA"] == aoa)]
-        df.loc[-1]
+    df["CL_strut_cor"] = df["CL"] - df_strut_aoa["CL"]
+    df["CD_strut_cor"] = df["CD"] - df_strut_aoa["CD"]
+    df["CMpitch25c_strut_cor"] = df["CMpitch25c"] - df_strut_aoa["CMpitch"]
+    return df
+
+def main():
+    add_modeloff_corrections()
 
 if __name__ == "__main__":
     main()
