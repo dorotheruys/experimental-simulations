e0c8758159b5c69477a13ab84e73c2bf7799d559
mvandongeren <martijnvd23@gmail.com>
2024-03-27 22:20:10 +0100

    Lot of stuff

--- Corrections/Corrections_main.py
+++ Corrections/Corrections_main.py
@@ -2,16 +2,23 @@ import pandas as pd
 
 from Corrections.Support_tare_correction import strut_correction
 from Corrections.Lift_interference import lift_interference
-
+from Corrections.Blockages import *
 from General.Pathfinder import get_file_path
+from General.Drag_coefficients import *
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 def corrections_combined(df):
-    df_with_strut = strut_correction(df)
-    print(df_with_strut)
+    df_dragthrust_coefficients = CD_CT(df)
 
-    lift_interference_cor = lift_interference(df_with_strut)
-    print(lift_interference_cor)
+    df_with_strut = strut_correction(df_dragthrust_coefficients)
+    #print(df_with_strut)
+
+    df_with_bcor = Full_blockage(df_with_strut)
+
+    lift_interference_cor = lift_interference(df_with_bcor)
+    #print(lift_interference_cor)
 
 
 def main():
--- General/Thrust_calculation.py
+++ General/Thrust_calculation.py
@@ -7,51 +7,27 @@ plt.rcParams.update({'font.size': 25})
 pd.set_option('display.max_rows', None)
 #pd.set_option('display.max_columns', None)
 
-def Thrust_estimation1(J,V,AoA):
-    #windmilling = df['rounded_J'] == 17
-    tunnel_velocity = df['rounded_v'] == V
-    prop_setting = df['rounded_J'] == J
-    aoa_setting = df['rounded_AoA'] == AoA
-    #windmilling_df = df.loc[(windmilling) & (tunnel_velocity) & (aoa_setting)].copy()
-    #windmilling_drag = windmilling_df['CD'].iloc[0]
-    windmilling_df = Windmilling_dragcoefficients()
-    windmilling_df = windmilling_df[(windmilling_df['rounded_AoA'] == AoA)].copy()
-    windmilling_drag = windmilling_df['CD_windmilling'].iloc[0]
-    filtered_df = df.loc[(prop_setting) & (tunnel_velocity) & (aoa_setting)].copy()
-    tancoef = filtered_df['CD'].iloc[0]
-    return -(tancoef-windmilling_drag)/np.cos(AoA*np.pi/180)
+# Old stuff
+# def Thrust_estimation1(J,V,AoA):
+#     #windmilling = df['rounded_J'] == 17
+#     tunnel_velocity = df['rounded_v'] == V
+#     prop_setting = df['rounded_J'] == J
+#     aoa_setting = df['rounded_AoA'] == AoA
+#     #windmilling_df = df.loc[(windmilling) & (tunnel_velocity) & (aoa_setting)].copy()
+#     #windmilling_drag = windmilling_df['CD'].iloc[0]
+#     windmilling_df = Windmilling_dragcoefficients()
+#     windmilling_df = windmilling_df[(windmilling_df['rounded_AoA'] == AoA)].copy()
+#     windmilling_drag = windmilling_df['CD_windmilling'].iloc[0]
+#     filtered_df = df.loc[(prop_setting) & (tunnel_velocity) & (aoa_setting)].copy()
+#     tancoef = filtered_df['CD'].iloc[0]
+#     return -(tancoef-windmilling_drag)/np.cos(AoA*np.pi/180)
 
-def Thrust_estimation(J,V,AoA):
-    tunnel_velocity = df['rounded_v'] == V
-    prop_setting = df['rounded_J'] == J
-    aoa_setting = df['rounded_AoA'] == AoA
-    curve = drag_interpolation()
-    windmilling_drag = curve(AoA)
-    filtered_df = df.loc[(prop_setting) & (tunnel_velocity)].copy()
-    tancoef = filtered_df['CD'].values
-    short_aoa = filtered_df['rounded_AoA'].values
-    #Interpolate CD data for all angles of attack
-    tunnel_prop_combi = [[{'rounded_v': V}, {'rounded_J': J}]]
-    CD_alpha_function = get_function_from_dataframe(df, 10, 'AoA', 'CD', tunnel_prop_combi, np.linspace(-6, 20, 26),None, None)
-    CD_array = CD_alpha_function[0].poly_coeff(np.arange(-5, 14.1, 1))
-    aoa = np.arange(-5,14.1,1)
-    #plt.plot(short_aoa)
-    # plt.plot(aoa, curve(aoa), label='Windmill')
-    # plt.scatter(short_aoa, tancoef)
-    # plt.plot(aoa,CD_array, label='Not windmill')
-    # plt.legend()
-    lst_coef = []
-    for angle in aoa:
-        tancoef = CD_array[int(angle+5)]
-        thrust_coefficient = -(tancoef-curve(angle))/np.cos(AoA*np.pi/180)
-        lst_coef.append(tancoef)
-    #plt.plot(aoa,lst_coef, label='thrust coefficient')
-    return thrust_coefficient, CD_array[int(AoA+5)]
-
-def Windmilling_dragcoefficients():
+def Windmilling_dragcoefficients(V):
     first_advance_ratio = df['rounded_J'] == 1.6
     upper_advance_ratio = df['rounded_J'] == 1.8
-    initial_guess = 2.415
+    V40_guess = 2.43                            #(2.359+2.5)/2
+    V20_guess = 2.33                            #(2.359+2.5)/2-0.1
+    V10_guess = 2.28                            #(2.359+2.5)/2-0.15
     lower_advance_ratio = df['rounded_J'] == 3.5
     tunnel_velocity = df['rounded_v'] == 40
     first_advance_regime = df.loc[(first_advance_ratio) & (tunnel_velocity)]
@@ -66,14 +42,43 @@ def Windmilling_dragcoefficients():
     if len(upper_resulting_CD)!=len(lower_resulting_CD):
         print('Lengths are not the same')
     else:
+        slope_lst = []
         for i in range(0,len(upper_resulting_CD)):
             slope = (upper_resulting_CD.iloc[i]-lower_resulting_CD.iloc[i])/(1.8-3.5)
-            CD_windmilling = upper_resulting_CD.iloc[i]+slope*(initial_guess-1.8)       #2.415 is the average windmilling advance ratio between experimental and relational
+            slope_lst.append(slope)
+            CD_windmilling = upper_resulting_CD.iloc[i]+slope*(V40_guess-1.8)       #2.415 is the average windmilling advance ratio between experimental and relational
             lst_CD_windmilling.append(CD_windmilling)
     windmilling_df = pd.DataFrame({
         'rounded_AoA': upper_linear_regime['rounded_AoA'].reset_index(drop=True),
         'CD_windmilling': lst_CD_windmilling
     })
+    aoa_lst = [-5, 7, 12, 14]
+    if V!=40:
+
+        lst_difference = []
+        lst_CD = []
+        i=0
+        slow = df.loc[(df['rounded_J'] == 1.6) & (df['rounded_v'] == V)]
+        slow_CD = slow['CD']
+        for aoa in aoa_lst:
+            baseline = first_resulting_CD.iloc[int(aoa+5)]
+            if V==10:
+                CD = slow_CD.iloc[int(i)]
+                lst_difference.append(baseline - CD - slope_lst[i] * (V40_guess - V10_guess))   #Also account for different J for windmilling
+                lst_CD.append(CD)
+            elif V==20:
+                CD = slow_CD.iloc[int(aoa+5)]
+                lst_difference.append(baseline-CD-slope_lst[i]*(V40_guess-V20_guess))           #Also account for different J for windmilling
+                lst_CD.append(CD)
+            i += 1
+        #plt.scatter(1.6, slow_CD.iloc[2])
+        #plt.scatter(aoa_lst, lst_CD)
+        windmilling_df['CD_windmilling'] = windmilling_df['CD_windmilling'] - lst_difference
+    J = [1.6, 1.8, V40_guess, 3.5]
+    CD_w = windmilling_df['CD_windmilling'].values
+    if V==40:
+        CD = [first_resulting_CD.iloc[17], upper_resulting_CD.iloc[1], CD_w[1], lower_resulting_CD.iloc[1]]
+        #plt.scatter(aoa_lst,[first_resulting_CD.iloc[0],first_resulting_CD.iloc[12],first_resulting_CD.iloc[17],first_resulting_CD.iloc[19]])
 
     # CD_nowindmill = [first_resulting_CD.iloc[1], upper_resulting_CD.iloc[1], lower_resulting_CD.iloc[1]]
     # J = [1.6,1.8,3.5]
@@ -85,20 +90,21 @@ def Windmilling_dragcoefficients():
     # plt.show()
     return windmilling_df
 
+# Vlst = [40,20,10]
+# for V in Vlst:
+#     windmill_df = Windmilling_dragcoefficients(V)
+#     aoa = windmill_df['rounded_AoA'].values
+#     CD_windmill = windmill_df['CD_windmilling'].values
+#     plt.plot(aoa,CD_windmill)
+# plt.show()
 
-
-def drag_interpolation():
-    # windmilling = df['rounded_J'] == 17
-    # tunnel_velocity = df['rounded_v'] == V
-    # windmilling_df = df.loc[(windmilling) & (tunnel_velocity)].copy()
-    # windmilling_drag = windmilling_df['CD'].values
-    # windmilling_aoa = windmilling_df['rounded_AoA'].values
-    windmilling_df = Windmilling_dragcoefficients()
+def drag_interpolation(V):
+    windmilling_df = Windmilling_dragcoefficients(V)
     windmilling_drag = windmilling_df['CD_windmilling'].values
     windmilling_aoa = windmilling_df['rounded_AoA'].values
     coefficients = np.polyfit(windmilling_aoa, windmilling_drag, deg=4)
     fitted_curve = np.poly1d(coefficients)
-    aoa = df['rounded_AoA'].unique()
+    # aoa = df['rounded_AoA'].unique()
     # plt.scatter(windmilling_aoa, windmilling_drag)
     # plt.plot(aoa, fitted_curve(aoa), color='red', label='Fitted Curve')
     # plt.xlabel('AoA')
@@ -106,47 +112,54 @@ def drag_interpolation():
     # plt.show()
     return fitted_curve
 
-print(Windmilling_dragcoefficients())
+
+def Thrust_estimation(J, V, AoA):
+    # Old stuff
+    # tunnel_velocity = df['rounded_v'] == V
+    # prop_setting = df['rounded_J'] == J
+    # aoa_setting = df['rounded_AoA'] == AoA
+    # filtered_df = df.loc[(prop_setting) & (tunnel_velocity)].copy()
+    # tancoef = filtered_df['CD'].values
+    # short_aoa = filtered_df['rounded_AoA'].values
+
+    # Find willing drag coefficients
+    curve = drag_interpolation(V)
+    windmilling_drag = curve(AoA)
+
+    # Interpolate CD data for all angles of attack
+    tunnel_prop_combi = [[{'rounded_v': V}, {'rounded_J': J}]]
+    CD_alpha_function = get_function_from_dataframe(df, 10, 'AoA', 'CD', tunnel_prop_combi, np.linspace(-6, 20, 26),
+                                                    None, None)
+    CD_array = CD_alpha_function[0].poly_coeff(np.arange(-5, 14.1, 1))
+    tancoef = CD_array[int(AoA + 5)]
+    thrust_coefficient = -(tancoef - windmilling_drag) / np.cos(AoA * np.pi / 180)
+
+    # Plotting stuff
+    # aoa = np.arange(-5,14.1,1)
+    # plt.plot(aoa, curve(aoa), label='Windmill')
+    # plt.scatter(short_aoa, tancoef)
+    # plt.plot(aoa,CD_array, label='Not windmill')
+    # plt.legend()
+    # lst_coef = []
+    # for angle in aoa:
+    #     tancoef = CD_array[int(angle+5)]
+    #     thrust_coefficient = -(tancoef-curve(angle))/np.cos(AoA*np.pi/180)
+    #     lst_coef.append(tancoef)
+    # plt.plot(aoa,lst_coef, label='thrust coefficient')
+    return thrust_coefficient
 
 
-J = [1.6,1.8,3.5]
-CT = []
-coef_lst = []
-for j in J:
-    CT.append(Thrust_estimation(j,40,7)[0])
-    coef_lst.append(Thrust_estimation(j,40,7)[1])
-coefficients = np.polyfit(coef_lst, J, deg=2)
-fitted_curve = np.poly1d(coefficients)
-J_array = np.linspace(-0.0045,0.13,100)
-plt.plot(J_array,fitted_curve(J_array))
-plt.scatter(coef_lst, J)
-plt.xlabel('Advance ratio')
-plt.ylabel('Drag coefficient from BAL_forces.m')
-plt.show()
-
-#print(Thrust_estimation(3.5,40,5))
-#Append thrust coefficient to dataframe
-# for index, row in df.iterrows():
-#     rounded_J = row['rounded_J']
-#     rounded_v = row['rounded_v']
-#     rounded_AoA = row['rounded_AoA']
-#
-#     new_value = Thrust_estimation1(rounded_J, 40, rounded_AoA)
-#     new_value = "{:.10f}".format(new_value)
-#     df.at[index, 'Thrust coefficient'] = new_value
-#
-# print(df)
-#
-# # Specify the file path where you want to save the CSV file
-# csv_file_path = 'Sort_data/bal_neg15_corrected.csv'
-#
-# # Use the to_csv() method to save the dataframe to a CSV file
-# df.to_csv(csv_file_path, index=False)
-
-
-#Old code
-# aoa_lst = [-5,7,12,14]
-#
-# for aoa in aoa_lst:
-#     print(Thrust_estimation1(1.6,40, aoa))
-#     print(Thrust_estimation2(1.6, 40, aoa))
+# J = [1.6,1.8,3.5]
+# CT = []
+# coef_lst = []
+# for j in J:
+#     CT.append(Thrust_estimation(j,40,7)[0])
+#     coef_lst.append(Thrust_estimation(j,40,7)[1])
+# coefficients = np.polyfit(coef_lst, J, deg=2)
+# fitted_curve = np.poly1d(coefficients)
+# J_array = np.linspace(-0.0045,0.13,100)
+# plt.plot(J_array,fitted_curve(J_array))
+# plt.scatter(coef_lst, J)
+# plt.xlabel('Advance ratio')
+# plt.ylabel('Drag coefficient from BAL_forces.m')
+# plt.show()
