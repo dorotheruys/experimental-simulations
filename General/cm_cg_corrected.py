a211a3f710bcec18862addd36a28b2f4ff6b902e
dorotheruys <dorothe@beruys.be>
2024-03-30 10:15:25 +0100

    finished the trim point calculations

--- General/cm_cg_corrected.py
+++ General/cm_cg_corrected.py
@@ -7,50 +7,49 @@ import pandas as pd
 from General.data_function_maker import get_function_set
 
 
-def get_decoupled_lift(df_data: pd.DataFrame, df_data_tailoff: pd.DataFrame, tunnel_speed: int):
+def get_decoupled_lift(df_data: pd.DataFrame, df_data_tailoff: pd.DataFrame, tunnel_speed: [int, float]):
     """
     A function that calculates the total lift, lift due to wing and lift due to horizontal tailplane
-    :param df_data: Data from the windtunnel
-    :param df_data_tailoff: Data from the tail off experiment
+    :param df_data: Data from the windtunnel on one tunnel speed!
+    :param df_data_tailoff: Data from the tail off experiment for the relevant tunnel speed only!
     :param tunnel_speed: tunnel speed
-    :return: a dataframe containing: AoA, tunnel speed, propeller speed, total CL, tail-off CL and CL due to horizontal tailplane
+    :return: the input dataframe but with added columns of CL_wing and CL_tail
     """
-    # Get the relevant data based on the tunel speed
-    df_total_relevant_data = get_function_set(df_data, {'rounded_v': tunnel_speed}, None)
 
     # Extract the lift coefficient from the tail-off data and interpolate
     df_wing = df_data_tailoff[['AoA', 'CL']]
     CL_wing_poly_coeff = np.poly1d(np.polyfit(df_wing['AoA'], df_wing['CL'], 2))
 
     # Construct a new dataframe including a tail-off lift coefficient for the AoA in the data and lift coefficient for the tail
-    # merged_df = df_total_relevant_data
-    df_total_relevant_data['CL_wing'] = CL_wing_poly_coeff(df_total_relevant_data['AoA'])
-    df_total_relevant_data['CL_tail'] = df_total_relevant_data['CL'] - df_total_relevant_data['CL_wing']
+    df_data['CL_wing'] = CL_wing_poly_coeff(df_data['AoA'])
+    df_data['CL_tail'] = df_data['CL cor'] - df_data['CL_wing']
 
-    # Make a pretty dataframe for output
-    # result_df = merged_df[['AoA', 'rounded_v', 'V', 'rounded_J', 'delta_e', 'CL', 'CL_wing', 'CL_tail']]
-    df_total_relevant_data = df_total_relevant_data.rename(columns={'CL': 'CL_total'})
-    return df_total_relevant_data
+    df_data = df_data.rename(columns={'CL cor': 'CL_total cor'})
+    return df_data
 
 
-def get_cm_cg_corrected(df_data: pd.DataFrame, df_data_tailoff: pd.DataFrame, tunnel_speed: [int, float], arm_ac_wing: float, arm_ac_ht: float, arm_cg: float, mac_wing: float):
+def get_cm_cg_corrected(df_data: pd.DataFrame, df_data_tailoff: list, arm_ac_wing: float, arm_ac_ht: float, arm_cg: float, mac_wing: float):
     """
-    A function to get the pitch model coefficient for a specific tunnel velocity due to lift and virtually added weight
+    A function to get the pitch model coefficient for all tunnel velocity due to lift and virtually added weight
     :param df_data: Wind tunnel data
-    :param df_data_tailoff: tail-off data for a specific tunnel speed
-    :param tunnel_speed:
+    :param df_data_tailoff: tail-off data for all tunnel speeds
     :param arm_ac_wing: chord-wise location aerodynamic centre main wing
     :param arm_ac_ht: chord-wise location aerodynamic centre horizontal tailplane
     :param arm_cg: chord-wise location centre of gravity
     :param mac_wing: reference model weight
-    :return: dataframe consisting of 'rounded_AoA', 'rounded_v', 'rounded_J', 'CL_total', 'CL_tailoff', 'CL_HT' and 'CM_vir'
+    :return:
     """
-    df_data_result = get_decoupled_lift(df_data, df_data_tailoff, tunnel_speed)
+    df_data_lst = []
+    for i, tunnel_speed in enumerate([10., 20., 40.]):
+        velocity_relevant_data = get_function_set(df_data, {'V': tunnel_speed}, None)
+        df_data_decoupled_lift = get_decoupled_lift(velocity_relevant_data, df_data_tailoff[i], tunnel_speed)
+        df_data_lst.append(df_data_decoupled_lift)
+    df_data_complete = pd.concat(df_data_lst, axis=0)
 
-    df_data_result['CM_due_to_lift'] = -(arm_ac_wing / mac_wing) * df_data_result['CL_wing'] - (arm_ac_ht / mac_wing) * df_data_result['CL_tail']
-    df_data_result['CM_0.25c_total'] = df_data_result['CM_due_to_lift'] + (arm_cg / mac_wing) * df_data_result['CL_total']
+    df_data_complete['CM_due_to_lift'] = -(arm_ac_wing / mac_wing) * df_data_complete['CL_wing'] - (arm_ac_ht / mac_wing) * df_data_complete['CL_tail']
+    df_data_complete['CM_0.25c_total'] = df_data_complete['CM_due_to_lift'] + (arm_cg / mac_wing) * df_data_complete['CL_total cor']
 
-    return df_data_result
+    return df_data_complete.sort_index()
 
 
 def get_cm_cg_cor_all_elevator(tailoff_data: list, bal_data: list, arm_ac_w: float, arm_ac_ht: float, arm_cg: float, ref_chord: float):
@@ -64,16 +63,8 @@ def get_cm_cg_cor_all_elevator(tailoff_data: list, bal_data: list, arm_ac_w: flo
     :param ref_chord: reference chord length to non-dimensionalise the moment (eg: MAC of the wing)
     :return: a dataframe consisting of new columns CL_wing, CL_tail, CM due to lift, total CM (the cg corrected one)
     """
-    CM_ref_lst = []
-    for i, V in enumerate([10, 20, 40]):
-        relevant_tailoff_data = tailoff_data[i]
-        CM_ref_15 = get_cm_cg_corrected(bal_data[2], relevant_tailoff_data, V, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
-        CM_ref_min15 = get_cm_cg_corrected(bal_data[0], relevant_tailoff_data, V, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
-        CM_ref_0 = get_cm_cg_corrected(bal_data[1], relevant_tailoff_data, V, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
+    CM_ref_15 = get_cm_cg_corrected(bal_data[2], tailoff_data, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
+    CM_ref_min15 = get_cm_cg_corrected(bal_data[0], tailoff_data, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
+    CM_ref_0 = get_cm_cg_corrected(bal_data[1], tailoff_data, arm_ac_w, arm_ac_ht, arm_cg, ref_chord)
 
-        CM_ref_complete = pd.concat([CM_ref_min15, CM_ref_0, CM_ref_15], axis=0)
-        CM_ref_lst.append(CM_ref_complete)
-
-    CM_ref_result = pd.concat(CM_ref_lst, axis=0)
-
-    return CM_ref_result
+    return pd.concat([CM_ref_min15, CM_ref_0, CM_ref_15], axis=0)
--- General/trim-point.py
+++ General/trim-point.py
@@ -19,68 +19,70 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from General.data_function_maker import get_function_from_dataframe, extract_from_list_classes, get_function_set
+from General.data_function_maker import get_function_from_dataframe, get_function_set
 from Corrections.Lift_interference import df_velocity_filter_tailoff
 from General.cm_cg_corrected import get_cm_cg_cor_all_elevator
-from Corrections.Corrections_main import corrections_combined
+# from Corrections.Corrections_main import corrections_combined
 
 
-def get_correct_data_based_on_elevator(elevator_defl: int, tunnel_prop_combi):
-    if elevator_defl == -15:
-        return get_function_from_dataframe(bal_sorted_min15, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
-    elif elevator_defl == 0:
-        return get_function_from_dataframe(bal_sorted_0, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
-    elif elevator_defl == 15:
-        return get_function_from_dataframe(bal_sorted_15, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
-    else:
-        print("This elevator deflection is not in the database.")
-        return
+# def get_correct_data_based_on_elevator(elevator_defl: int, tunnel_prop_combi):
+#     if elevator_defl == -15:
+#         return get_function_from_dataframe(bal_sorted_min15, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
+#     elif elevator_defl == 0:
+#         return get_function_from_dataframe(bal_sorted_0, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
+#     elif elevator_defl == 15:
+#         return get_function_from_dataframe(bal_sorted_15, 2, 'AoA', 'CL', tunnel_prop_combi, np.linspace(-6, 20, 26), None, None)
+#     else:
+#         print("This elevator deflection is not in the database.")
+#         return
 
 
-def get_aoa_from_cl(elevator_defl: int, cl_des: float, tunnel_speed: int, propeller_speed: float, tunnel_prop_combi):
-    """
-    A function that extracts the angle of attack for a given lift coefficients for a combination between
-    tunnel speed, propeller speed and elevator deflection.
-    :param elevator_defl: input elevator deflection in degrees (options: -15, 0, 15)
-    :param cl_des:
-    :param tunnel_speed:
-    :param propeller_speed:
-    :param tunnel_prop_combi:
-    :return:
-    """
+# def get_aoa_from_cl(elevator_defl: int, cl_des: float, tunnel_speed: int, propeller_speed: float, tunnel_prop_combi):
+#     """
+#     A function that extracts the angle of attack for a given lift coefficients for a combination between
+#     tunnel speed, propeller speed and elevator deflection.
+#     :param elevator_defl: input elevator deflection in degrees (options: -15, 0, 15)
+#     :param cl_des:
+#     :param tunnel_speed:
+#     :param propeller_speed:
+#     :param tunnel_prop_combi:
+#     :return:
+#     """
+#
+#     CL_alpha_function = get_correct_data_based_on_elevator(elevator_defl, tunnel_prop_combi)
+#
+#     # Extract the poly coefficients from the relevant class and make a function
+#     poly1d_func = extract_from_list_classes(CL_alpha_function, tunnel_speed, propeller_speed)
+#     AoA_range = np.linspace(-6, 20, 200)
+#     function = poly1d_func(AoA_range)
+#
+#     # Get the intersections between y = CL and the functions
+#     intersections = []
+#     for j, CL in enumerate(function):
+#         if abs(CL - cl_des) < 0.001:
+#             intersections.append([AoA_range[j], CL])
+#         else:
+#             continue
+#     intersections_array = np.array(intersections)
+#
+#     return intersections_array
 
-    CL_alpha_function = get_correct_data_based_on_elevator(elevator_defl, tunnel_prop_combi)
 
-    # Extract the poly coefficients from the relevant class and make a function
-    poly1d_func = extract_from_list_classes(CL_alpha_function, tunnel_speed, propeller_speed)
-    AoA_range = np.linspace(-6, 20, 200)
-    function = poly1d_func(AoA_range)
+def find_trim_points_per_aoa(data, aoa, order):
+    relevant_set = get_function_set(data, {'AoA': aoa}, None)
+    functionclass_CM_lst = get_function_from_dataframe(relevant_set, order, 'delta_e', 'CM_0.25c_total', prop_tunnel_combis, np.linspace(-20, 20, 200), None, None)
 
-    # Get the intersections between y = CL and the functions
-    intersections = []
-    for j, CL in enumerate(function):
-        if abs(CL - cl_des) < 0.001:
-            intersections.append([AoA_range[j], CL])
-        else:
-            continue
-    intersections_array = np.array(intersections)
+    for CM_function in functionclass_CM_lst:
+        roots_x = np.roots(CM_function.poly_coeff.coefficients)
+        # roots_y = [CM_function.poly_coeff(root) for root in roots_x]
+        # if len(roots_x) > 1:
+        #     test = [[roots_x[i], roots_y[i]] for i in range(len(roots_x))]
+        # else:
+        #     test = [roots_x[0], roots_y[0]]
 
-    return intersections_array
+        CM_function.trim_points = roots_x
 
-
-def find_trim_points_per_aoa(data, order):
-
-    for aoa in [-5, 7, 12, 14]:
-        relevant_set = get_function_set(data, {'AoA': aoa}, None)
-        functionclass_lst = get_function_from_dataframe(relevant_set, order, 'delta_e', 'CM_0.25c_total', prop_tunnel_combis, np.linspace(-20, 20, 50), None, None)
-        root_lst = []
-        for item in functionclass_lst:
-            roots_x = np.polynomial.polynomial.polyroots(item.poly_coeff)
-            roots_y = [item.poly_coeff(root) for root in roots_x]
-            test = range(len(roots_x))
-            root_lst.append([[roots_x[i], roots_y[i]] for i in range(len(roots_x))])
-            # item.trim_point = roots
-    return
+    return functionclass_CM_lst
 
 
 # Initialize
@@ -100,17 +102,21 @@ l_cg = (0.35 - 0.25) * MAC_w
 
 if __name__ == "__main__":
     # Get the data
-    bal_sorted_min15 = pd.read_csv('../Sort_data/bal_sorted1.csv')
-    bal_sorted_min15 = pd.concat([bal_sorted_min15, pd.DataFrame({'delta_e': [-15]*len(bal_sorted_min15)})], axis=1)
-    data_corrected_min15 = corrections_combined(bal_sorted_min15)
+    # bal_sorted_min15 = pd.read_csv('../Sort_data/bal_sorted1.csv')
+    # bal_sorted_min15 = pd.concat([bal_sorted_min15, pd.DataFrame({'delta_e': [-15]*len(bal_sorted_min15)})], axis=1)
+    # data_corrected_min15 = corrections_combined(bal_sorted_min15)
 
-    bal_sorted_0 = pd.read_csv('../Sort_data/bal_sorted2.csv')
-    bal_sorted_0 = pd.concat([bal_sorted_0, pd.DataFrame({'delta_e': [0]*len(bal_sorted_0)})], axis=1)
-    data_corrected_0 = corrections_combined(bal_sorted_0)
+    # bal_sorted_0 = pd.read_csv('../Sort_data/bal_sorted2.csv')
+    # bal_sorted_0 = pd.concat([bal_sorted_0, pd.DataFrame({'delta_e': [0]*len(bal_sorted_0)})], axis=1)
+    # data_corrected_0 = corrections_combined(bal_sorted_0)
 
-    bal_sorted_15 = pd.read_csv('../Sort_data/bal_sorted3.csv')
-    bal_sorted_15 = pd.concat([bal_sorted_15, pd.DataFrame({'delta_e': [15]*len(bal_sorted_15)})], axis=1)
-    data_corrected_15 = corrections_combined(bal_sorted_15)
+    # bal_sorted_15 = pd.read_csv('../Sort_data/bal_sorted3.csv')
+    # bal_sorted_15 = pd.concat([bal_sorted_15, pd.DataFrame({'delta_e': [15]*len(bal_sorted_15)})], axis=1)
+    # data_corrected_15 = corrections_combined(bal_sorted_15)
+
+    data_corrected_min15 = pd.read_csv('../Corrections/data_cor_min15.csv')
+    data_corrected_0 = pd.read_csv('../Corrections/data_cor_0.csv')
+    data_corrected_15 = pd.read_csv('../Corrections/data_cor_15.csv')
 
     # Get the tail-off data
     data_tailoff_40 = df_velocity_filter_tailoff(40)
@@ -118,19 +124,25 @@ if __name__ == "__main__":
     # data_tailoff_40.insert(1, 'rounded_AoA', rounded_AoA_40)
 
     data_tailoff_20 = df_velocity_filter_tailoff(20)
-    rounded_AoA_20 = data_tailoff_20['AoA'].round()
-    data_tailoff_20.insert(1, 'rounded_AoA', rounded_AoA_20)
+    # rounded_AoA_20 = data_tailoff_20['AoA'].round()
+    # data_tailoff_20.insert(1, 'rounded_AoA', rounded_AoA_20)
 
     # Determine the corrected CM for all tunnel velocities
     tail_off_data = [data_tailoff_20, data_tailoff_20, data_tailoff_40]
     CM_cg_cor = get_cm_cg_cor_all_elevator(tail_off_data, [data_corrected_min15, data_corrected_0, data_corrected_15], l_ac_w, l_ac_ht, l_cg, MAC_w)
 
-    # Find the trim points
-    find_trim_points_per_aoa(CM_cg_cor, 1)
+    # Find the trim points.
+    CM_cg_cor_relevant = get_function_set(CM_cg_cor, {'AoA': 7}, None)
+    CM_function_AOA7_lst = find_trim_points_per_aoa(CM_cg_cor_relevant, 7, 1)
 
     # Plot CM vs delta_e for AoA = 7
-    CM_cg_cor_relevant = get_function_set(CM_cg_cor, {'AoA': 7}, None)
     CM_cg_cor_function_lst = get_function_from_dataframe(CM_cg_cor_relevant, 1, 'delta_e', 'CM_0.25c_total', prop_tunnel_combis, np.linspace(-20, 20, 50), f'$\\delta_e$ [deg]', f'$C_M$ [-]')
 
     plt.plot([-20, 20], [0, 0], color='0')
+
+    for CMfunction in CM_function_AOA7_lst:
+        test = CMfunction.trim_points
+        plt.scatter(CMfunction.trim_points[0], CMfunction.trim_points[1], color='0')
+
+
     plt.show()
