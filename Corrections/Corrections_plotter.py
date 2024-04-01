fdf48ab42d753e9963d9c97ad671b6aecf0b8275
mvandongeren <martijnvd23@gmail.com>
2024-04-01 14:00:32 +0200

    Correction plots

--- Corrections/Corrections_plotter.py
+++ Corrections/Corrections_plotter.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from Plotting.plotter import PlotData
 from General.Data_sorting import specific_old_file, specific_cor_file
+from Sort_data.Remove_windmill_data import *
 import matplotlib.pyplot as plt
 
 def aoa_CL(V,J):
@@ -94,18 +95,56 @@ def CD_CL(V,J):
     CL_cor = df_cor['CL cor'].values
     CD_cor = df_cor['CD cor'].values
 
-    x_range_array = CD_old
+    x_range_array = CL_old
 
-    adjustment = 0.1 * max(x_range_array)
+    adjustment = 0.05 * max(x_range_array)
 
     # Adjust the range
     # range = [x_range_array[0] - adjustment] + [x + adjustment / (len(x_range_array) - 2) for x in x_range_array[1:-1]] + [x_range_array[-1] + adjustment]
     range = np.linspace(x_range_array[0] - adjustment, x_range_array[-1] + adjustment, 100)
 
-    PlotData('CD', 'CL', range, [CD_old, CL_old, CD_cor, CL_cor], 'curvefit', ['Uncorrected', 'Corrected'])
+    # a, b, c = np.polyfit(CL_old, CD_old, deg=2)
+    #
+    # plot_inverse_second_degree_polynomial(a, b, c, x_range_array)
+    # plt.scatter(CD_old,CL_cor)
+
+    PlotData('CL', 'CD', range, [CL_old, CD_old, CL_cor, CD_cor], 'curvefit', ['Uncorrected', 'Corrected'])
     return
 
-J = [1.6, 1.8, 3.5]
-for j in J:
-    CD_CL(40, j)
-plt.show()
+def J_CT(V,AoA):
+    df_cor = specific_cor_file()
+
+    df_old = df_cor.loc[(df_cor['rounded_v'] == V) & (df_cor['rounded_AoA'] == AoA)]
+    J_old = df_old['rounded_J'].values
+    CT_old = df_old['Thrust coefficient'].values
+
+    V_low = V - 1
+    V_high = V + 1
+
+    df_cor = df_cor .loc[(df_cor['V cor'] >= V_low) & (df_cor['V cor'] <= V_high) & (df_cor['rounded_AoA'] == AoA)]
+    J_cor = df_cor['rounded_J'].values
+    CT_cor = df_cor['CT cor'].values
+
+    x_range_array = J_old
+
+    adjustment = 0.05 * max(x_range_array)
+
+    # Adjust the range
+    # range = [x_range_array[0] - adjustment] + [x + adjustment / (len(x_range_array) - 2) for x in x_range_array[1:-1]] + [x_range_array[-1] + adjustment]
+    range = np.linspace(x_range_array[0] - adjustment, x_range_array[-1] + adjustment, 100)
+
+    # a, b, c = np.polyfit(CL_old, CD_old, deg=2)
+    #
+    # plot_inverse_second_degree_polynomial(a, b, c, x_range_array)
+    # plt.scatter(CD_old,CL_cor)
+
+    PlotData('J', 'CT', range, [J_old, CT_old, J_cor, CT_cor], 'lists', ['Uncorrected', 'Corrected'])
+    return
+
+# J = [1.6]
+# for j in J:
+#     CD_CL(40, j)
+# aoa = [7]
+# for angle in aoa:
+#     J_CT(40, angle)
+# plt.show()
--- Sort_data/Remove_windmill_data.py
+++ Sort_data/Remove_windmill_data.py
@@ -10,34 +10,61 @@
 #
 # # Save the DataFrame as a CSV file
 # filtered_df.to_csv(file_path, index=False)
+import numpy as np
+import matplotlib.pyplot as plt
+#
 # import numpy as np
 # import matplotlib.pyplot as plt
-# from scipy.optimize import curve_fit
 #
-# # Original data
-# x = np.array([1, 2, 3, 4, 5])
-# y = np.array([1, np.sqrt(2)+0.1, np.sqrt(3)+0.1, np.sqrt(4)+0.1, np.sqrt(5)])  # Example data for square root fit
+# def inverse_second_degree_polynomial(a, b, c, x_values):
+#     def inverse_roots(x):
+#         discriminant = b**2 - 4*a*(c - x)
+#         if discriminant < 0:
+#             return None, None  # No real roots
+#         else:
+#             root1 = (-b + np.sqrt(discriminant)) / (2*a)
+#             root2 = (-b - np.sqrt(discriminant)) / (2*a)
+#             return root1, root2
 #
-# for deg in degree:
-#     # Define the square root function
-# sqrt_func = lambda x, a: a * x**(deg)
+#     # Calculate the inverse roots for each x value
+#     inverse_roots_list = [inverse_roots(x) for x in x_values]
 #
-# # Perform the curve fit
-# params, covariance = curve_fit(sqrt_func, x, y)
+#     # Filter out points with no real roots
+#     inverse_roots_list = [(root1, root2) for root1, root2 in inverse_roots_list if root1 is not None and root2 is not None]
 #
-# # Plot the original data
-# plt.scatter(x, y, label='Original Data')
+#     # Separate the roots into root1 and root2
+#     root1_values = [roots[0] for roots in inverse_roots_list]
+#     root2_values = [roots[1] for roots in inverse_roots_list]
 #
-# # Plot the square root fit
-# x_fit = np.linspace(min(x), max(x), 100)
-# y_fit = sqrt_func(x_fit, *params)
-#     plt.plot(x_fit, y_fit, color='red', label='Square Root Fit')
+#     return root1_values, root2_values
+#
+# def plot_inverse_second_degree_polynomial(a, b, c, x_range):
+#     # Generate x values
+#     x_values = np.linspace(x_range[0], x_range[-1], 100)
+#     # Calculate the inverse function
+#     root1_values, root2_values = inverse_second_degree_polynomial(a, b, c, x_range)
+#     #print(root1_values,root2_values)
+#     x_values1 = x_range[:len(root1_values)][::-1]
+#     x_values2 = x_range[:len(root2_values)]
+#     root1_values = root1_values[::-1]
+#     x = []
+#     for xv in x_values1:
+#         x.append(xv)
+#     for xv in x_values2:
+#         x.append(xv)
+#     y = []
+#     for yv in root1_values:
+#         y.append(yv)
+#     for yv in root2_values:
+#         y.append(yv)
+#
+#     plt.plot(x,y)
 #
 #     plt.xlabel('X-axis')
 #     plt.ylabel('Y-axis')
-#     plt.title('Square Root Fit')
+#     plt.title('Inverse Function of a Second-Degree Polynomial')
 #     plt.legend()
 #     plt.grid(True)
-# plt.show()
-#
-# print("Square root fit parameter (a):", params)
+#     #plt.show()
+
+#plot_inverse_second_degree_polynomial(0.11319508, -0.0440425, 0.00109855, np.linspace(0,1,11))
