5444f540d6a816bf0be7f8e7147a06ed09ea8f60
mvandongeren <martijnvd23@gmail.com>
2024-04-03 17:38:26 +0200

    Correction figures and comments

--- General/Thrust_calculation.py
+++ General/Thrust_calculation.py
@@ -177,17 +177,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# J = [1.6,1.8,3.5]
-# n = [123.02, 109.36, 56.24]
-# CT = []
-# coef_lst = []
-# for j in J:
-#     CT.append(Thrust_estimation(j,40,7))
-# # coefficients = np.polyfit(coef_lst, J, deg=2)
-# # fitted_curve = np.poly1d(coefficients)
-# # J_array = np.linspace(-0.0045,0.13,100)
-# plt.plot(J,CT)
-# plt.xlabel('Advance ratio')
-# plt.ylabel('Thrust coefficient')
-# plt.show()
